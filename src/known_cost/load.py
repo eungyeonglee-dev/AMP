b99b3d0664c341c18487a310a25b97662fd29c02
eungyeonglee-dev <anhanmu78@gmail.com>
2024-05-13 16:08:25 +0900

    compare same profile db with diff profile db

---
+++ src/known_cost/load.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+data_1 = np.load('gpt2XL_A10_1.npy')
+print(data_1)
+print(len(data_1))
+
+# transformer_db = data_1[2]
+# data_1[2:26] = transformer_db
+
+# print(data_1)
+# print(len(data_1))
+
+
+# data_1 = np.load('gpt2_G4_2.npy')
+# print(data_1)
+# print(len(data_1))
+
+# data_1 = np.load('gpt2_G4_4.npy')
+# print(data_1)
+# print(len(data_1))
+
+# data_1 = np.load('gpt2_P3_1.npy')
+# print(data_1)
+# print(len(data_1))
+
+# data_1 = np.load('gpt2_P3_2.npy')
+# print(data_1)
+# print(len(data_1))
+
+# data_1 = np.load('gpt2_P3_4.npy')
+# print(data_1)
+# print(len(data_1))
---
+++ src/known_cost/make_nparray.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# make transformer layer profile db equaly
+# A10
+# A10_1 = np.random.rand(50)/1000
+# A10_2 = np.random.rand(50)/1000 
+# A10_3 = np.random.rand(50)/1000 
+
+# np.save('gpt2XL_A10_1.npy', A10_1)
+# np.save('gpt2XL_A10_2.npy', A10_2)
+# np.save('gpt2XL_A10_4.npy', A10_3)
+
+# # # A100
+# A100_1 = np.random.rand(50)/1000 
+# A100_2 = np.random.rand(50)/1000 
+# A100_3 = np.random.rand(50)/1000 
+
+# np.save('gpt2XL_A100_1.npy', A100_1)
+# np.save('gpt2XL_A100_2.npy', A100_2)
+# np.save('gpt2XL_A100_4.npy', A100_3)
+
+# data_1 = np.load('gpt2XL_A100_1.npy')
+# print(data_1)
+# print(len(data_1))
+
+# transformer_db = data_1[2]
+# data_1[2:26] = transformer_db
+
+# print(data_1)
+# print(len(data_1))
